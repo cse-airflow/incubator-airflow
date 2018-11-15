47a4462f53a105cd9a7c21fb5e8cc864f1b1b266
Tom Miller <tmiller@microsoft.com>
2018-11-14 23:49:03 -0800

    Removing unneeded test

--- tests/contrib/hooks/test_azure_cosmos_hook.py
+++ tests/contrib/hooks/test_azure_cosmos_hook.py
@@ -191,50 +191,5 @@ class TestAzureCosmosDbHook(unittest.TestCase):
         cosmos_mock.assert_any_call(self.test_end_point, {'masterKey': self.test_master_key})
         cosmos_mock.assert_has_calls(expected_calls)
 
-    def simple_roundtrip_test(self): 
-        hook = AzureCosmosDBHook(azure_cosmos_conn_id='azcosmos_test_key_id') 
-        self.assertEqual(hook.conn_id, 'azcosmos_test_key_id') 
-        # Create a new data base 
-        db_name = "AirflowTempDB" 
-        coll_name = "AirflowTempCollection" 
-        hook.create_database(db_name) 
-        hook.create_collection(coll_name, db_name) 
-        total_documents = hook.get_documents("SELECT * FROM c", db_name, coll_name) 
-        logging.getLogger().info(total_documents)
-        before_count = len(total_documents) 
-        hook.upsert_document({ 
-            "id": "airflow_test_item", 
-            "temp": "stuff"}, db_name, coll_name) 
-        total_documents_after = hook.get_documents("SELECT * FROM c", db_name, coll_name) 
-        logging.getLogger().info(total_documents_after)
-        logging.getLogger().info(type(total_documents_after))
-        logging.getLogger().info(type(total_documents_after[0]))
-        logging.getLogger().info(total_documents_after[0])
-        after_count = len(total_documents_after) 
-        self.assertEqual(before_count + 1, after_count) 
-        created_document = hook.get_document('airflow_test_item', db_name, coll_name) 
-        self.assertEqual(created_document['temp'], "stuff") 
-        hook.delete_document("airflow_test_item", db_name, coll_name) 
-        total_documents_after_delete = hook.get_documents("SELECT * FROM c", db_name, coll_name) 
-        after_delete_count = len(total_documents_after_delete) 
-        self.assertEqual(before_count, after_delete_count) 
-        insert_new_documents = [ 
-            {"id": "airflow_test_item", "temp": "stuff"}, 
-            {"id": "airflow_test_item1", "temp": "stuff2"}, 
-            {"id": "airflow_test_item2", "temp": "stuff3"}] 
-        hook.insert_documents(insert_new_documents, db_name, coll_name) 
-        self.assertIsNotNone(hook.get_document("airflow_test_item", db_name, coll_name)) 
-        self.assertIsNotNone(hook.get_document("airflow_test_item1", db_name, coll_name)) 
-        self.assertIsNotNone(hook.get_document("airflow_test_item2", db_name, coll_name)) 
-        total_documents_after_multiple_insert = hook.get_documents("SELECT * FROM c", db_name, coll_name) 
-        after_multiple_insert_count = len(total_documents_after_multiple_insert) 
-        self.assertEqual(before_count + 3, after_multiple_insert_count) 
-        hook.delete_document("airflow_test_item", db_name, coll_name) 
-        hook.delete_document("airflow_test_item1", db_name, coll_name) 
-        hook.delete_document("airflow_test_item2", db_name, coll_name) 
-        hook.delete_collection(coll_name, db_name) 
-        hook.delete_database(db_name)
-        #raise AirflowException()
-
 if __name__ == '__main__':
     unittest.main()
